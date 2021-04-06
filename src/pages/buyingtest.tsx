b081fa6a5012e9ae159ce410111ae777202a8378
perlpark <hello@jinjoo.me>
2021-04-07 01:40:36 +0900

    Inputs -> Input

--- src/pages/buyingtest.tsx
+++ src/pages/buyingtest.tsx
@@ -1,5 +1,5 @@
 import { Layout } from "components/layout";
-import { Colors, Gradient, Button, Inputs, BuyProduct } from "components/ui";
+import { Colors, Gradient, Button, Input, BuyProduct } from "components/ui";
 
 export default function Home() {
   console.log(Colors, Gradient);
@@ -12,9 +12,9 @@ export default function Home() {
       <br/> <br/>
       <Button type = "start">시작하기</Button>
       <br/> <br/>
-      <Inputs/>
+      <Input/>
       <br/> <br/>    
-      <Inputs isDisable = {true}/>
+      <Input isDisable = {true}/>
       <br/> <br/>
       <Buying discount={100} price={50000}/>
       <br/> <br/>
