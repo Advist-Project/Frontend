482bf7c96e2f68b474db1c4a030a008ac004202e
ympark99 <ympark1230@naver.com>
2021-04-11 23:15:29 +0900

    구글 로그인 테스트1

--- src/pages/testpage.tsx
+++ src/pages/testpage.tsx
@@ -1,5 +1,24 @@
-export default function testPage() {
-    return (
-        <div>testpage</div>
-    )    
-}
+import React from "react";
+import axios from "axios";
+
+const Users = ({}) => {
+
+    return(
+        <>
+        <h1>{Users.name}</h1>
+        </>
+    )
+    };
+  
+  Users.getInitialProps = async () => {
+    const res = await axios.get('https://criel.herokuapp.com/user/getuser');
+    console.log(res);
+    console.log("data loaded");
+
+    return{
+        users : res.data
+    }
+
+  };
+  
+  export default Users;
