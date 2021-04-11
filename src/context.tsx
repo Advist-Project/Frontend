4f0b1ea1a413bd1e91be06974f27cd5456bf2af1
ympark99 <ympark1230@naver.com>
2021-04-11 21:42:57 +0900

    브랜치 테스트

---
+++ src/context.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useEffect, useState } from 'react'
+import axios from 'axios';
+import { AxiosResponse } from 'axios';
+
+export default function Context(props: any) {
+
+    const [userObject, setUserObject] = useState<any>();
+
+    useEffect(() => {
+        axios.get("https://criel.herokuapp.com/user/getuser", { withCredentials: true }).then((res: AxiosResponse) => {
+            console.log(res);
+            if (res.data) {
+                setUserObject(res.data);
+            }
+        })
+    }, [])
+    return (
+        <myContext.Provider value={userObject}>{props.children}</myContext.Provider>
+    )
+}
+export const myContext = createContext({});
