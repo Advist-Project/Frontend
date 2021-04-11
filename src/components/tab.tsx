3d7b76f110f172773bca2d8bc2356e3094850207
perlpark <hello@jinjoo.me>
2021-04-12 01:47:32 +0900

    상세 페이지 탭 수정 2 (이미지 깜빡임 해결)

--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -8,6 +8,14 @@ interface IAnchorTabProps {
   active: string;
 }
 const AnchorTab: React.FC<IAnchorTabProps> = ({ create, active }) => {
+
+  const tab:{[key:string]: any} = {
+    workbook: { value: "워크북", icon: '/icon/workbook_64p.svg', icon_active: '/icon/workbook_64p.svg', sectionRef: '', ...create.workbook },
+    coaching: { value: "코칭", icon: '/icon/coach_gray_64p.svg', icon_active: '/icon/coach_64p.svg', sectionRef: '', ...create.coaching },
+    review: { value: "리뷰", icon: '/icon/review_gray_64p.svg', icon_active: '/icon/review_64p.svg', sectionRef: '', ...create.review },
+    ask: { value: "문의", icon: '/icon/ask_gray_64p.svg', icon_active: '/icon/ask_64p.svg', sectionRef: '', ...create.ask }
+  }
+
   const AnchorTab = styled.div`
     height: 109px;
     background: ${Colors.white};
@@ -27,26 +35,59 @@ const AnchorTab: React.FC<IAnchorTabProps> = ({ create, active }) => {
     text-align: center;
     line-height: 24px;
 
-    img {
+    .icon {
       display: block;
+      width: 64px;
+      height: 64px;
       margin: 6px auto 0;
-      opacity: 0.6;
     }
+
+    &.workbook {
+      .icon {
+        background-image: url(${tab.workbook.icon});
+      }
+    }
+    &.coaching {
+      .icon {
+        background-image: url(${tab.coaching.icon});
+      }
+    }
+    &.review {
+      .icon {
+        background-image: url(${tab.review.icon});
+      }
+    }
+    &.ask {
+      .icon {
+        background-image: url(${tab.ask.icon});
+      }
+    }
+
 
     &.active {
       border-bottom: 8px ${Colors.secondary} solid;
-
-      img {
-        opacity: 0.8;
+    }
+    &.active.workbook {
+      .icon {
+        background-image: url(${tab.workbook.icon_active});
+      }
+    }
+    &.active.coaching {
+      .icon {
+        background-image: url(${tab.coaching.icon_active});
+      }
+    }
+    &.active.review {
+      .icon {
+        background-image: url(${tab.review.icon_active});
+      }
+    }
+    &.active.ask {
+      .icon {
+        background-image: url(${tab.ask.icon_active});
       }
     }
   `;
-  const tab:{[key:string]: any} = {
-    workbook: { value: "워크북", icon: '/icon/workbook_64p.svg', icon_active: '/icon/workbook_64p.svg', sectionRef: '', ...create.workbook },
-    coaching: { value: "코칭", icon: '/icon/coach_gray_64p.svg', icon_active: '/icon/coach_64p.svg', sectionRef: '', ...create.coaching },
-    review: { value: "리뷰", icon: '/icon/review_gray_64p.svg', icon_active: '/icon/review_64p.svg', sectionRef: '', ...create.review },
-    ask: { value: "문의", icon: '/icon/ask_gray_64p.svg', icon_active: '/icon/ask_64p.svg', sectionRef: '', ...create.ask }
-  }
 
   return (
     <AnchorTab>
@@ -54,8 +95,8 @@ const AnchorTab: React.FC<IAnchorTabProps> = ({ create, active }) => {
         <ul>
           {
             Object.keys(create).map((type, i) => (
-              <Tab key={i} className={active === tab[type].sectionRef ? 'active' : ''}>
-                <img src={tab[type].icon_active}/>
+              <Tab key={i} className={`${type} ${active === tab[type].sectionRef ? 'active' : ''}`}>
+                <div className="icon"/>
                 {tab[type].value}
               </Tab>
           ))}
