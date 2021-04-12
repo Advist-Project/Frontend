535b53f8bba95738da319e9706593448084c8c1f
ympark99 <ympark1230@naver.com>
2021-04-12 19:58:42 +0900

    google test4

--- src/pages/testpage.tsx
+++ src/pages/testpage.tsx
@@ -1,12 +1,16 @@
 import React, { useContext } from 'react'
 import { myContext } from './login';
 import { User } from '../types/logintypes';
+import {NextApiRequest, NextApiResponse} from 'next';
 
-export default function Homepage() {
+
+export default function Homepage(req : NextApiRequest, res : NextApiResponse) {
 
     const userObject = useContext(myContext) as User;
     console.log(userObject);
     console.log(userObject.username);
+    console.log(req.body);
+    console.log(res);
 
     return (
         <div>
