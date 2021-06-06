e05d1389750b9d3109ad98e5783f163711c37525
perlpark <hello@jinjoo.me>
2021-06-06 21:04:47 +0900

    기획전 desktop 완료

---
+++ src/components/product-card-list2.tsx
@@ -0,0 +1,26 @@
+import { ProductCard } from "./product-card";
+
+interface IProductListProps {
+  data: any[];
+}
+export const ProductList: React.FC<IProductListProps> = ({ data }) => { 
+  console.log(data);
+
+  return (
+    <>
+      {data.map((item, num) => ( 
+        <ProductCard
+          key = {num}
+          id={item.itemId}      
+          label={item.label}
+          title={item.title}
+          likes={item.likes}
+          // img={item.img}
+          img={`/detail/${item.itemId}/thumb.png`}
+          tag={item.tag}
+          price={item.price}
+          discountPrice={item.discountPrice} />
+        ))}
+      </>
+  )
+}
--- src/pages/all.tsx
+++ src/pages/all.tsx
@@ -1,9 +1,9 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import React from "react";
 import { Layout } from "components/layout";
-import { Colors } from "components/ui";
-import { ProductList } from "components/product-card-list";
+import { ProductList } from "components/product-card-list2";
 import styled from "@emotion/styled";
+import Image from 'next/image';
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const res = await fetch(process.env.NEXT_PUBLIC_API_URL as string +`/exhibition`);
@@ -20,24 +20,31 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }
 }
 export default function All({exhibition}: InferGetServerSidePropsType<typeof getServerSideProps>){
-  console.log(exhibition);
 
   return (
-    <Layout>
+    <Layout whiteHeader>
       <Jumbotron>
-        <div className="wrap">
-          당신이 꿈꾸는 커리어를 만드는 방법,<br/>
-          업계 선배가 1:1로 알려드려요
-        </div>
+        <p className="wrap">
+          당신이 꿈꾸는 커리어를 만드는 방법, <br/>
+          <Strong>업계 선배가 1:1로 알려드려요</Strong>
+        </p>
+        <Image
+          src="/thumb.png"
+          alt=""
+          width={1920}
+          height={432}
+        />
       </Jumbotron>
       <div className="wrap">
       {
         exhibition.map((item: any) => (
           <Exhibition key={item._id}>
-            <Title>{item.title}</Title>
-            <Desc>{item.subTitle}</Desc>
+            <Title>
+              <img src="/icon/coach_64p.svg" style={{marginRight: '6px'}}/>
+              {item.title}
+            </Title>
             <ProductListWrap>
-              <ProductList data={[item.itemInfo]} />
+              <ProductList data={item.itemInfo} />
             </ProductListWrap>
           </Exhibition>
         ))
@@ -48,25 +55,35 @@ export default function All({exhibition}: InferGetServerSidePropsType<typeof get
 }
 
 const Jumbotron = styled.div`
-  background: ${Colors.gray4};
-  padding: 80px 0;
-  font-size: 1.5rem;
-  line-height: 1.5em;
-  margin-bottom: 80px;
+  padding-top: calc(156px + 48px);
+  background: url('/allPage_bg.png') center bottom/100% auto no-repeat;
+  text-align: center;
+  margin-bottom: 112px;
+
+  img {
+    display: block;
+  }
+  p {
+    color: #F3EFFF;
+    font-size: 20px;
+    line-height: 150%;
+  }
+`;
+
+const Strong = styled.span`
+  font-weight: 700;
 `;
 
 const Exhibition = styled.div`
-  margin-bottom: 80px;
+  margin-bottom: calc(156px - 32px);
 `;
 
 const Title = styled.h2`
-  font-size: 1.5rem;
+  font-size: 2.25rem;
   font-weight: 700;
-  margin-bottom: 1em;
-`;
-
-const Desc = styled.p`
-  margin-bottom: 2em;
+  margin-bottom: 80px;
+  
+  img { vertical-align: middle; }
 `;
 
 const ProductListWrap = styled.div`
