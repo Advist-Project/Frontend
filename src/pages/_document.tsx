1e36d6b4603d2892352d6a1da2e2c749f2f18561
perlpark <hello@jinjoo.me>
2021-06-10 00:19:30 +0900

    애널리틱스 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
         <Head>
           <script src="https://cdn.bootpay.co.kr/js/bootpay-3.3.1.min.js" type="application/javascript"></script>
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-MDTQ5709C8"></script>
-          <script src="common/analy.js" type="application/javascript"></script>
+          <script src="/analy.js" type="application/javascript"></script>
         </Head>
         <body>
           <Main />
