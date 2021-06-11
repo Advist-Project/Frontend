72fc7105a4f7bfe5be562b8b9ab130c920c3cdb5
perlpark <hello@jinjoo.me>
2021-06-11 23:57:06 +0900

    오픈 그래프 타이틀, 썸네일 추가

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -13,12 +13,15 @@ export const Layout: React.FC<ILayoutProps> = ({ children, title, noFooter, whit
   return (
     <>
       <Head>
-        <title>
-        { title ?
-          `${title} | ${APP_NAME}`
-          : `업계 일잘러 선배들의 1:1 코칭, ${APP_NAME}`
-        }  
-        </title>
+        { title
+          ? <title>{title} | {APP_NAME}</title>
+          : <title>업계 일잘러 선배들의 1:1 코칭, {APP_NAME}</title>
+        }
+        { title
+          ? <meta property="og:title" content={title} />
+          : <meta property="og:title" content={`업계 일잘러 선배들의 1:1 코칭, ${APP_NAME}`} />
+        }
+        <meta property="og:image" content="/mainThumb.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <Header white={whiteHeader}/>
